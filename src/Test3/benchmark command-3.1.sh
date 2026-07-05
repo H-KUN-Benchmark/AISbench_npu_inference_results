acdb285c22a9066025ba9dd4c824747de1cd9e69
AISbench <kevinotw@gmail.com>
2026-07-05 11:37:21 +0800

    Add files via upload

---
+++ src/Test3/benchmark command-3.1.sh
@@ -0,0 +1,1 @@
+python3 aisbench_test.py --input_len 50 --output_len 200 --data_num 4 --concurrency 1
---
+++ src/Test3/benchmark command-3.3.sh
@@ -0,0 +1,2 @@
+python3 aisbench_test.py  --input_len 1 --output_len 700 --data_num 4 --concurrency 1
+
